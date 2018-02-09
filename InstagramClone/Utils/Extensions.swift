4d3f244d6745613c39a9ec98b9efb2248b426473
h01m3s <lin426797@gmail.com>
2018-02-09 01:36:50 -0500

    UIColor and Anchor Extension

---
+++ InstagramClone/Utils/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  InstagramClone
+//
+//  Created by Weijie Lin on 2/9/18.
+//  Copyright © 2018 Weijie Lin. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+}
