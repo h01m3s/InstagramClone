90f973776ec54f65b313c32460c099cca4a11445
h01m3s <lin426797@gmail.com>
2018-03-07 00:11:45 -0500

    Home Feed Image Caching

--- InstagramClone/Utils/CustomImageView.swift
+++ InstagramClone/Utils/CustomImageView.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
+var imageCache = [String: UIImage]()
+
 class CustomImageView: UIImageView {
     
     var lastURLUsedToLoadImage: String?
     
     func loadImage(urlString: String) {
         lastURLUsedToLoadImage = urlString
+        
+        if let cachedImage = imageCache[urlString] {
+            self.image = cachedImage
+            return
+        }
+        
         guard let url = URL(string: urlString) else { return }
+        
         URLSession.shared.dataTask(with: url) { (data, response, err) in
             
             if let err = err {
@@ -27,10 +36,12 @@ class CustomImageView: UIImageView {
             }
             
             guard let imageData = data else { return }
-            let image = UIImage(data: imageData)
+            let photoImage = UIImage(data: imageData)
+            
+            imageCache[url.absoluteString] = photoImage
             
             DispatchQueue.main.async {
-                self.image = image
+                self.image = photoImage
             }
             
             }.resume()
