b77593e9fe7d3c3720cbfa2f14cf84efe49c05a4
h01m3s <lin426797@gmail.com>
2018-03-16 01:06:13 -0400

    Camera Custom Transitions

--- InstagramClone/Camera/CameraController.swift
+++ InstagramClone/Camera/CameraController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
+class CameraController: UIViewController, AVCapturePhotoCaptureDelegate, UIViewControllerTransitioningDelegate {
     
     let dismissButton: UIButton = {
         let button = UIButton(type: .system)
@@ -84,9 +84,21 @@ class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupCaptureSession()
+        transitioningDelegate = self
         
+        setupCaptureSession()
         setupHUD()
+    }
+    
+    let customAnimationPresentor = CustomAnimationPresentor()
+    let customAnimationDismisser = CustomAnimationDismisser()
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        return customAnimationPresentor
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return customAnimationDismisser
     }
     
     override var prefersStatusBarHidden: Bool {
